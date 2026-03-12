72645f73f5f41713030406c4fbbbc260ecd04cac
Anatou <anatole@desnot.com>
2026-03-12 16:16:20 +0100

    changed README and edited i-docker

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Repo contenant les fichiers servant à mettre en place le SI fictif du projet Orion-HCL de mars 2026
 
 Les scripts sont fait pour marcher sur Debian
+
+Installer docker sur une machine vierge
+```
+wget -O - http://tiny.cc/7090101/i-docker.sh | bash
+# Long version: https://github.com/Anatou/projet-orion-hcl/raw/refs/heads/main/get-docker.sh
+```
---
+++ i-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y curl
+
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+if [ "$(id -u)" != "0" ]; then
+	# Setup rootless (docker for user)
+	sudo apt install -y uidmap
+	dockerd-rootless-setuptool.sh install
+	echo "export DOCKER_HOST=unix:///run/user/1000/docker.sock" >> $HOME/.bashrc
+	export DOCKER_HOST=unix:///run/user/1000/docker.sock
+fi
